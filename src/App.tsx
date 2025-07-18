577122a7bce0c255888204791ce6d4b32a8b8bea
William-Dias-Marcos <william.diasmarcos@gmail.com>
2025-07-17 22:39:06 -0300

    feat: added functionality to generate qrcode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "./components/ui/input";
+import { QRCodeCanvas } from "qrcode.react";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("https://portfolio-wdm.vercel.app/");
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6">
+      <QRCodeCanvas value={text} />
+      <div>
+        <Input
+          type="text"
+          placeholder="Seu texto"
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <Button type="submit" variant="outline">
+        Baixar
+      </Button>
     </div>
   );
 }
