7df5762d30256c6e51b7fbb65bcb6d15be3a514f
William-Dias-Marcos <william.diasmarcos@gmail.com>
2025-07-18 07:08:24 -0300

    feat: add select theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import { Button } from "@/components/ui/button";
 import { Input } from "./components/ui/input";
 import { QRCodeCanvas } from "qrcode.react";
 import { useState } from "react";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   const [text, setText] = useState("https://portfolio-wdm.vercel.app/");
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6">
-      <QRCodeCanvas value={text} />
-      <div>
-        <Input
-          type="text"
-          placeholder="Seu texto"
-          onChange={(e) => setText(e.target.value)}
-        />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6">
+        <QRCodeCanvas value={text} marginSize={2} />
+        <div>
+          <Input
+            type="text"
+            placeholder="Seu texto"
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <Button type="submit" variant="outline">
+          Baixar
+        </Button>
       </div>
-      <Button type="submit" variant="outline">
-        Baixar
-      </Button>
-    </div>
+    </ThemeProvider>
   );
 }
 
